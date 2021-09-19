7e847900454cf5234bce81521c9c8dde22321d4d
Tanish0712 <Tanishlahoti502@gmail.com>
2021-09-19 12:56:42 +0400

    first commit

--- hofvidz/settings.py
+++ hofvidz/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h8_7npvn1ymmvkup4j-)l1)g@hn2s8h(@lg_ebvc2=chg@i0h-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://ancient-ridge-15559.herokuapp.com','hallofvidz.com','www.hallofvidz.com']
+ALLOWED_HOSTS = []
 
 
 LOGIN_URL = 'login'
@@ -83,7 +83,10 @@ WSGI_APPLICATION = 'hofvidz.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
-    # Hall
+        # Hall
     path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
     path('halloffame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
     path('halloffame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
